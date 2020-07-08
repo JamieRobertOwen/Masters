fd1ac7a3b124f1d70f1f1655851d414c1ce0cd56
Jamie <jamierobertowen@outlook.com>
2020-07-08 16:31:17 +1000

    fixing bad code

--- MIP.jl
+++ MIP.jl
@@ -1,22 +1,27 @@
-using JuMP, GLPK, CSV, DataFrames
+using JuMP, CSV, DataFrames, GLPK
 #JuMP to construct problem
 #GLPK as solver
 #CSV for data imput and output
 #DataFrames for type
 """ first pass at implimenting MIP """
+
 NumPatients = 20
 numSpecialists = 6
 appointmentLengths = [1,2,3]
 numAppoints = 80
-lengthOfDay =50;
-clinicMIP = Model(GlPK.Optimizer)
+lengthOfDay = 50
+
+TestInfo = CSV.read("testData.csv")
+
+
+clinicMIP = Model(GLPK.Optimizer)
+
 
 @variable(clinicMIP, strictlyLater[1:numAppoints,1:numAppoints], Bin)
-@variable(clinicMIP, 0<=startingTime[1:numAppoints]<=lengthOfDay,Int)
+@variable(clinicMIP, 0<=startingTime[1:numAppoints]<=lengthOfDay, Int)
 
-@constraints(clinicMIP, begin
-    #con 1 -
-end)
+
+@constraint(clinicMIP, constraint[j=1:2], sum(A[j,i]*x[i] for i=1:3) <= b[j])
 
 
 #MVP - maximise patients scheduled
